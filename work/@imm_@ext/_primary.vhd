6b56c419554f9afc4e8c9c480e43bb2b22ea5e86
cellulis <109467160+yrtc99@users.noreply.github.com>
2025-01-08 23:08:57 +0800

    Delete work directory

--- work/@imm_@ext/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Imm_Ext is
-    port(
-        inst            : in     vl_logic_vector(31 downto 0);
-        imm_ext_out     : out    vl_logic_vector(31 downto 0)
-    );
-end Imm_Ext;
